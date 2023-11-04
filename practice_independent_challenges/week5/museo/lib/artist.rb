ce6643b3f321e4dedc465e72edfa895db44558f9
Ashley <ashuhleyt@gmail.com>
2023-11-03 18:37:33 -0700

    really really trying to remember the basics

--- practice_independent_challenges/week5/museo/lib/artist.rb
+++ practice_independent_challenges/week5/museo/lib/artist.rb
@@ -1,8 +1,17 @@
 class Artist
-  attr_reader :id, :name
+  attr_reader :id, :name, :born, :died, :country
 
   def initialize(attributes)
     @id = attributes[:id]
     @name = attributes[:name]
+    @born = attributes[:born]
+    @died = attributes[:died]
+    @country = attributes[:country]
+  end
+
+  def age_at_death
+    birth_year = @born.to_i
+    death_year = @died.to_i
+    death_year - birth_year
   end
 end
--- practice_independent_challenges/week5/museo/spec/artist_spec.rb
+++ practice_independent_challenges/week5/museo/spec/artist_spec.rb
@@ -1,18 +1,27 @@
-# require './lib/artist.rb'
+ require './lib/artist.rb'
 
-# RSpec.describe Artist do 
-#   before :each do 
-#     @attributes1 = {
-#       id: "1",
-#       name: "Rue Mouffetard, Paris (Boy with Bottles)",
-#       artist_id: "4",
-#       year: "1954"
-#     } 
+ RSpec.describe Artist do 
+   before :each do 
+     @attributes = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
 
-#     @photograph = Photograph.new(@attributes1)
-#   end
+     @artist = Artist.new(@attributes)
+   end
 
-#   it 'exists' do 
-#     expect(@photograph.id).to eq("1")
-#   end
-# end
+   it 'exists with attributes' do 
+     expect(@artist.id).to eq("2")
+     expect(@artist.name).to eq("Ansel Adams")
+     expect(@artist.born).to eq("1902")
+     expect(@artist.died).to eq("1984")
+     expect(@artist.country).to eq("United States")
+  end
+
+  it 'calculates their age at death' do 
+    expect(@artist.age_at_death).to eq(82)
+  end
+end
