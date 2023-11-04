13f50f4e62b3d55638c134ea84f0b1a9cd47309e
Ashley <ashuhleyt@gmail.com>
2023-11-03 18:27:22 -0700

    trying to remember the basics

--- practice_independent_challenges/week5/museo/spec/artist_spec.rb
+++ practice_independent_challenges/week5/museo/spec/artist_spec.rb
@@ -1,7 +1,18 @@
-require './lib/artist.rb'
+# require './lib/artist.rb'
 
-RSpec.describe Artist do 
-  before :each do 
-    @
-  end
-end
+# RSpec.describe Artist do 
+#   before :each do 
+#     @attributes1 = {
+#       id: "1",
+#       name: "Rue Mouffetard, Paris (Boy with Bottles)",
+#       artist_id: "4",
+#       year: "1954"
+#     } 
+
+#     @photograph = Photograph.new(@attributes1)
+#   end
+
+#   it 'exists' do 
+#     expect(@photograph.id).to eq("1")
+#   end
+# end
